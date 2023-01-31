5917ebe6e855923c32159986fa27dc552bce790b
amirhf <amirhacking85@gmail.com>
2023-01-31 16:24:38 +0330

    write device crud at device manager

--- src/Managers/DeviceManager.php
+++ src/Managers/DeviceManager.php
@@ -4,27 +4,42 @@ namespace dnj\ErrorTracker\Laravel\Server\Managers;
 
 use dnj\ErrorTracker\Contracts\IDevice;
 use dnj\ErrorTracker\Contracts\IDeviceManager;
+use dnj\ErrorTracker\Laravel\Server\Models\Device;
 
 class DeviceManager implements IDeviceManager
 {
-    // TODO
     public function search(array $filters): iterable
     {
-        // TODO: Implement search() method.
+        return Device::filter($filters)->get();
     }
 
     public function store(?string $title = null, ?array $extra = null, ?int $owner = null, bool $userActivityLog = false): IDevice
     {
-        // TODO: Implement store() method.
+        $device = new Device(
+            [
+                'title' => $title,
+                'extra' => $extra,
+                'owner' => $owner,
+            ]
+        );
+
+        $device->save();
+
+        return $device;
     }
 
     public function update(IDevice|int $device, array $changes, bool $userActivityLog = false): IDevice
     {
-        // TODO: Implement update() method.
+        $model = Device::query()->findOrFail($device);
+        $model->fill($changes);
+        $model->save();
+
+        return $model;
     }
 
     public function destroy(IDevice|int $device, bool $userActivityLog = false): void
     {
-        // TODO: Implement destroy() method.
+        $model = Device::query()->findOrFail($device);
+        $model->delete();
     }
 }
