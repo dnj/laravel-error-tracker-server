cd2677cccfec923f6df13c3f8a0f060d27c0993f
amirhf <amirhacking85@gmail.com>
2023-01-31 16:43:48 +0330

    make new resource for device

---
+++ src/Http/Resources/Device/UpdateResource.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace dnj\ErrorTracker\Laravel\Server\Http\Resources\Device;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UpdateResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
