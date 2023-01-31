5d8e7dd76980b982d78cd33748b28e88d0b76688
amirhf <amirhacking85@gmail.com>
2023-01-31 03:46:26 +0330

    make route for app

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use dnj\ErrorTracker\Laravel\Server\Http\Controllers\AppController;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api', SubstituteBindings::class])->group(function () {
-    // TODO
+    Route::group(['prefix' => 'app', 'as' => 'app.'], function () {
+        Route::get('/search', [AppController::class, 'search'])->name('search');
+        Route::post('/store', [AppController::class, 'store'])->name('store');
+        Route::put('/update/{id}', [AppController::class, 'update'])->name('update');
+        Route::delete('/destroy/{id}', [AppController::class, 'destroy'])->name('destroy');
+    });
 });
