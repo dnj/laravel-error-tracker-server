2bd641bc2f5ec2a9f8df98047618a94361700372
amirhf <amirhacking85@gmail.com>
2023-01-31 20:55:11 +0330

    call search function and destroy from log manager

--- src/Http/Controllers/LogController.php
+++ src/Http/Controllers/LogController.php
@@ -3,11 +3,51 @@
 namespace dnj\ErrorTracker\Laravel\Server\Http\Controllers;
 
 use dnj\ErrorTracker\Contracts\ILogManager;
+use dnj\ErrorTracker\Laravel\Server\Http\Requests\Log\SearchRequest;
+use dnj\ErrorTracker\Laravel\Server\Http\Resources\Log\SearchResource;
 
 class LogController extends Controller
 {
     public function __construct(protected ILogManager $logManager)
     {
     }
-    // TODO
+
+    public function search(SearchRequest $searchRequest)
+    {
+
+        $search = $this->logManager->search($searchRequest->only(
+            [
+                'apps',
+                'devices',
+                'levels',
+                'message',
+                'unread',
+                'user',
+            ]
+        ));
+
+        return new SearchResource($search);
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function markAsRead()
+    {
+
+    }
+
+    public function markAsUnRead()
+    {
+
+    }
+
+    public function destroy(int $id)
+    {
+        $this->logManager->destroy($id);
+    }
+
+
 }
