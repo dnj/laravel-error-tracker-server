734a7aa32042202b15375a9b8c8549d33ea9f2ed
amirhf <amirhacking85@gmail.com>
2023-01-31 19:58:45 +0330

    add route logs

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use dnj\ErrorTracker\Laravel\Server\Http\Controllers\AppController;
 use dnj\ErrorTracker\Laravel\Server\Http\Controllers\DeviceController;
+use dnj\ErrorTracker\Laravel\Server\Http\Controllers\LogController;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api', SubstituteBindings::class])->group(function () {
+
     Route::group(['prefix' => 'app', 'as' => 'app.'], function () {
         Route::get('/search', [AppController::class, 'search'])->name('search');
         Route::post('/store', [AppController::class, 'store'])->name('store');
@@ -18,5 +20,11 @@ Route::middleware(['api', SubstituteBindings::class])->group(function () {
         Route::post('/store', [DeviceController::class, 'store'])->name('store');
         Route::put('/update/{id}', [DeviceController::class, 'update'])->name('update');
         Route::delete('/destroy/{id}', [DeviceController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::group(['prefix' => 'log', 'as' => 'log.'], function () {
+        Route::get('/search', [LogController::class, 'search'])->name('search');
+        Route::post('/store', [LogController::class, 'store'])->name('store');
+        Route::delete('/destroy/{id}', [LogController::class, 'destroy'])->name('destroy');
     });
 });
