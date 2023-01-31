75be204dd69a73c58a303b53e41b6b5a9425061c
amirhf <amirhacking85@gmail.com>
2023-01-31 15:42:32 +0330

    improve api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use dnj\ErrorTracker\Laravel\Server\Http\Controllers\AppController;
+use dnj\ErrorTracker\Laravel\Server\Http\Controllers\DeviceController;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 
@@ -10,5 +11,12 @@ Route::middleware(['api', SubstituteBindings::class])->group(function () {
         Route::post('/store', [AppController::class, 'store'])->name('store');
         Route::put('/update/{id}', [AppController::class, 'update'])->name('update');
         Route::delete('/destroy/{id}', [AppController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::group(['prefix' => 'device', 'as' => 'device.'], function () {
+        Route::get('/search', [DeviceController::class, 'search'])->name('search');
+        Route::post('/store', [DeviceController::class, 'store'])->name('store');
+        Route::put('/update/{id}', [DeviceController::class, 'update'])->name('update');
+        Route::delete('/destroy/{id}', [DeviceController::class, 'destroy'])->name('destroy');
     });
 });
