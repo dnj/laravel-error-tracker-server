cff189cc908a9d86c9996971a83678458d921820
amirhf <amirhacking85@gmail.com>
2023-02-02 01:11:28 +0330

    write test for store section

--- tests/Feature/LogManagerTest.php
+++ tests/Feature/LogManagerTest.php
@@ -2,7 +2,10 @@
 
 namespace dnj\ErrorTracker\Laravel\Server\Tests\Feature;
 
-use dnj\ErrorTracker\Laravel\Server\Constants\LogLevelConstants;
+use Carbon\Carbon;
+use dnj\ErrorTracker\Contracts\LogLevel;
+use dnj\ErrorTracker\Laravel\Server\Models\App;
+use dnj\ErrorTracker\Laravel\Server\Models\Device;
 use dnj\ErrorTracker\Laravel\Server\Models\Log;
 use dnj\ErrorTracker\Laravel\Server\Tests\TestCase;
 use Illuminate\Testing\Fluent\AssertableJson;
@@ -18,7 +21,7 @@ class LogManagerTest extends TestCase
             [
                 'apps' => [1],
                 'devices' => [1],
-                'levels' => [LogLevelConstants::INFO],
+                'levels' => [LogLevel::INFO],
                 'message' => 'test',
                 'unread' => true,
                 // 'user' => '',
@@ -26,9 +29,46 @@ class LogManagerTest extends TestCase
         ));
 
         $response->assertStatus(ResponseAlias::HTTP_OK)
-        ->assertJson(function (AssertableJson $json) {
-            $json->etc();
-        });
+            ->assertJson(function (AssertableJson $json) {
+                $json->etc();
+            });
+    }
+
+    public function testCanStore(): void
+    {
+        $app = App::factory()->create();
+        $device = Device::factory()->create();
+
+        $data = [
+            'app' => $app->id,
+            'device' => $device->id,
+            'level' => LogLevel::INFO->name,
+            'message' => 'message test',
+            'data' => ['test_key edited' => 'test_value edited'],
+            'read' => ['userId' => 1, 'readAt' => Carbon::now()],
+        ];
+
+        $this->postJson(route('log.store'), $data)
+            ->assertStatus(ResponseAlias::HTTP_CREATED)
+            ->assertJson(function (AssertableJson $json) {
+                $json->etc();
+            });
+    }
+
+    public function testMarkAsRead()
+    {
+    }
+
+    public function testCanNotMarkAsRead()
+    {
+    }
+
+    public function testMarkAsUnRead()
+    {
+    }
+
+    public function testCanNotMarkAsUnRead()
+    {
     }
 
     public function testCanDestroy()
@@ -38,5 +78,4 @@ class LogManagerTest extends TestCase
         $this->deleteJson(route('device.destroy', ['id' => $app->id]))
             ->assertStatus(ResponseAlias::HTTP_OK);
     }
-
 }
